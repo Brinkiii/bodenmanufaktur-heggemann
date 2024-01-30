16d2ccf028a138f078466c89c48c0f0b04ae482b
Brinkiii <155560550+Brinkiii@users.noreply.github.com>
2024-01-30 20:00:06 +0100

    navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
   content: ["./src/**/*.{html,js}", "./node_modules/flowbite/**/*.js"],
   theme: {
     extend: {},
+    container: {
+      center: true,
+      padding: '8px',
+    }
   },
   plugins: [
     require('flowbite/plugin')
