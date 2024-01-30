ce5a17739c797fc448f1599006aa145471384ac9
Brinkiii <155560550+Brinkiii@users.noreply.github.com>
2024-01-30 22:48:24 +0100

    rough first draft except for footer finished

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,11 +5,12 @@ module.exports = {
     extend: {
       colors: {
         primary: {"50":"#eff6ff","100":"#dbeafe","200":"#bfdbfe","300":"#93c5fd","400":"#60a5fa","500":"#3b82f6","600":"#2563eb","700":"#1d4ed8","800":"#1e40af","900":"#1e3a8a","950":"#172554"}
-      }
+      },
     },
     container: {
       center: true,
       padding: '8px',
+      maxWidth: '1584px'
     },
     fontFamily: {
       'sans': 'Nunito Sans',
