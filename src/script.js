eafc05c90a6c3636b391c7647605ba22b8d8f93d
Brinkiii <155560550+Brinkiii@users.noreply.github.com>
2024-02-01 21:25:27 +0100

    responsive changes and footer added

---
+++ src/script.js
@@ -0,0 +1,19 @@
+var rellax = new Rellax('.rellax', {
+    speed: -2,
+    center: true,
+    wrapper: null,
+    round: true,
+    vertical: true,
+    horizontal: false
+});
+
+const lenis = new Lenis()
+    lenis.on('scroll', (e) => {
+        console.log(e)
+    })
+
+    function raf(time) {
+        lenis.raf(time)
+        requestAnimationFrame(raf)
+    }
+requestAnimationFrame(raf)
